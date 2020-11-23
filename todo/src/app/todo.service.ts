917fb1a3fbff76d775c8d23f374fc5e4012405c4
Surasak Srisawangwong <max.surasak@gmail.com>
2020-11-23 13:19:14 +0700

    Fixed api cors

--- todo/src/app/todo.service.ts
+++ todo/src/app/todo.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,13 +10,13 @@ export class TodoService {
   constructor(private http: HttpClient) {}
 
   getList(): Observable<any> {
-    return this.http.get('/api/todos');
+    return this.http.get(environment.API_URL + '/todos');
   }
 
   create(data): Observable<any> {
-    return this.http.post('/api/todos', { todo: data });
+    return this.http.post(environment.API_URL + '/todos', { todo: data });
   }
   update(id, data): Observable<any> {
-    return this.http.put('/api/todos/' + id, { todo: data });
+    return this.http.put(environment.API_URL + '/todos/' + id, { todo: data });
   }
 }
